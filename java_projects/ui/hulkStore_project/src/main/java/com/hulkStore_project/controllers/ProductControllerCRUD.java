7d7a97eb6edfeb63a04a648d13b2bd498d078956
OMAR <ojramirezv@gmail.com>
2021-12-10 21:50:32 -0500

    se conecta funcionalidad de insercion a interfaz de mantenimiento de producto

--- java_projects/ui/hulkStore_project/src/main/java/com/hulkStore_project/controllers/ProductControllerCRUD.java
+++ java_projects/ui/hulkStore_project/src/main/java/com/hulkStore_project/controllers/ProductControllerCRUD.java
@@ -2,11 +2,21 @@ package com.hulkStore_project.controllers;
 
 
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zul.Button;
+import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listitem;
 import org.zkoss.zul.Textbox;
+
+import com.hulkStore_project.model.ObjetoRespProduct;
 
 public class ProductControllerCRUD extends Controller {
 	Button btn_save_product;
@@ -16,6 +26,7 @@ public class ProductControllerCRUD extends Controller {
 	String product_name;
 	int stock;
 	int category_id;
+	private HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
 	
 @Override
 public void doAfterCompose(Component comp) throws Exception {
@@ -31,8 +42,21 @@ public class guardarListener implements EventListener {
     	product_name = txt_product_name.getValue();
     	category_id = Integer.parseInt(txt_category_id.getValue());
     	stock = Integer.parseInt(txt_stock.getValue());
-    	
+    	insertarProducto(product_name, category_id, stock);
     }
+}
+
+private void insertarProducto(String product_name, int category_id, int stock) {
+	final HttpRequest requestPost = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8888/hulkStore/InsertProduct?product_name="+product_name+"&category_id="+category_id+"&stock="+stock)).build();
+	try {
+		Listitem item,item1;
+		Listcell cell;
+		final HttpResponse<String> response = httpClient.send(requestPost, HttpResponse.BodyHandlers.ofString());
+		
+	} catch (IOException | InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 }
 
 }
