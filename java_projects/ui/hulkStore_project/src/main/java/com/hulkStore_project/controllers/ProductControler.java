732dd9123c2960c6fac73d28b9c37b2bd48a29d0
OMAR <ojramirezv@gmail.com>
2021-12-14 12:59:01 -0500

    se avanza funcionalidad de dar click sobre item de listbox

--- java_projects/ui/hulkStore_project/src/main/java/com/hulkStore_project/controllers/ProductControler.java
+++ java_projects/ui/hulkStore_project/src/main/java/com/hulkStore_project/controllers/ProductControler.java
@@ -10,8 +10,10 @@ import java.net.http.HttpResponse;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.ListModel;
@@ -27,12 +29,14 @@ import com.hulkStore_project.model.ObjetoRespProduct;
 import com.hulkStore_project.model.Product;
 
 
-public class ProductControler extends Controller,SelectorComposer<Window>{
+public class ProductControler extends Controller{
 	//private Label menu_1;
 	private Button btn_ver_productos;
 	private Listbox lbl_products;
 	Button btn_add_product;
-	private ListModel<Product> productModel;
+    private ListModel<Product> productModel;
+    @Wire
+    private Window win;
 	//Consumo rest
 	private HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
 	ObjetoRespProduct ObjRespProd=null;
@@ -45,12 +49,21 @@ public class ProductControler extends Controller,SelectorComposer<Window>{
 		btn_add_product.setLabel("Add");
 		cargarProductos();
 	}
+	private void showNotify(String msg,Component ref){
+        Clients.showNotification(msg,"info",ref,"top_right",2000);
+    }
+	
+	public ListModel<Product> getProductModel() {
+        return productModel;
+    }
 	
 	@Listen("onSelect = listbox")
-	public void eliminaProducto() {
-		Set<Product> selectedProduct = ((ListModelList<Product>)productModel).getSelection();
-		int size = selectedProduct.size();
-	}
+	public void actualizarMensaje() {
+        Set<Product> selectedProduct = ((ListModelList<Product>)productModel).getSelection();
+        int size = selectedProduct.size();
+        System.out.println("tamano "+size);
+        showNotify(size > 0 ? size + " product selected: " + selectedProduct : "no product selected", win);
+    }
 	
 	private void cargarProductos() {
 		final HttpRequest requestPost = HttpRequest.newBuilder().GET().uri(URI.create("http://localhost:8888/hulkStore/GetProducts")).build();
